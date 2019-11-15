0a59b3ffd5bde699b950984bcee6f4517bc3350b
Hassan Magdy <Hasan.Magdy@gmail.com>
2019-11-16 00:09:58 +0200

    add image-magick-example-zip-demo3

---
+++ option3_image-magick-example-npm_layer/index.js
@@ -0,0 +1,77 @@
+
+var aws = require('aws-sdk');
+var s3 = new aws.S3({apiVersion: '2006-03-01'});
+const imageThumbnail = require('image-thumbnail');
+
+
+/**
+ * Global DEBUG setting
+ * @type {boolean}
+ */
+const DEBUG = false;
+
+/**
+ *
+ * @type {number}
+ */
+const RESIZE_WIDTH = 150;
+
+lol = async function (event, context) {
+
+    if (DEBUG) {
+        console.log('Received event:', JSON.stringify(event, null, 2));
+    }
+
+    // Get the object from the event and show its content type
+    // var bucket = ''event.Records[0].s3.bucket.name;
+    // var key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
+    // Get the object from the event and show its content type
+    var bucket = 'mystaff-files';// event.Records[0].s3.bucket.name;
+
+    var key = decodeURIComponent('screenshot/VcKxVBrmYBMArp5C/XDZvDmjZnQE1rFYA/6C52F7C4-20A8-53D6-8147-C1888B7D0BAD/2019-11-05T17:28:33.000Z/1-original.jpg'.replace(/\+/g, ' '));
+
+
+
+    var params = {
+        Bucket: bucket,
+        Key: key
+    };
+
+    const image = await s3.getObject(params).promise();
+    console.log(image);
+    // const resizedImg = await sharp(image.Body).resize(RESIZE_WIDTH, RESIZE_WIDTH).max().toBuffer();
+
+
+    try {
+        let options = { width: RESIZE_WIDTH }
+        const resizedImg = await imageThumbnail(image.Body, options);
+        console.log(resizedImg);
+        // resize OK
+        var newObject = {
+            Bucket: bucket,
+            Key: key.replace('-original', '-small'),
+            Body: resizedImg,
+            ContentType: image.ContentType
+        };
+        const updated = await s3.putObject(newObject).promise();
+        if (DEBUG) {
+            console.log('resize - OK', newObject);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+
+
+};
+
+(async () => {
+    try {
+        await lol()
+        console.log('DONE');
+    } catch (e) {
+        console.log('HBL');
+        console.log(e);
+        // Deal with the fact the chain failed
+    }
+})();
